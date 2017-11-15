682a11208ead8ec4c3c535d50087a764597ef78e
amanda <amandavilelaalmeida@gmail.com>
2017-11-15 19:31:54 -0200

    Add after parties list

--- js/index.js
+++ js/index.js
@@ -1,9 +1,48 @@
-(function() {
+(async function() {
   if (document.querySelector('.index').length === 0) return;
 
   const addAfterPartyButton = document.querySelector('.add-after-party-button');
+  const afterList = document.querySelector('#after-list');
+
+
+  const afterPartyRequest = await fetch("https://after-party-21427.firebaseio.com/afters.json");
+  const responseData = await afterPartyRequest.json();
+
+  const afterPartyIds = Object.keys(responseData);
+  const afterParties = Object.values(responseData)
+                             .map((after, index) => {
+                                after.id = afterPartyIds[index];
+                                return after;
+                              });
+
+  localStorage.setItem("afterParties", JSON.stringify(responseData));
+
+  function getMostVotedVenue(venues) {
+    return venues.reduce((previous, current) => previous.votes > current.votes ? previous : current);
+  }
+
+  function createAfterPartiesMarkup(afterParties) {
+    return afterParties.map((after, index) => {
+        return `<li class="list__item">
+          <h2 class="list__item__title">${after.name}</h2>
+          <p class="list__item__detail">${getMostVotedVenue(after.venues).name}</p>
+          <button data-after-id="${after.id}" class="btn-vote list__item__button" type="button" name="button">vote</button>
+        </li>`
+    }).join("");
+  }
+
+  afterList.innerHTML = createAfterPartiesMarkup(afterParties);
 
   addAfterPartyButton.addEventListener('click', (ev) => {
     window.location.href = 'add-after-party.html';
   });
+
+  const voteButtons = document.querySelectorAll('.btn-vote');
+
+  voteButtons.forEach(b => {
+    b.addEventListener('click', function() {
+      localStorage.setItem("currentAfterParty", b.getAttribute("data-after-id"));
+      window.location.href = 'vote-for-a-venue.html';
+    });
+  })
 })();
